038102aa1abf3dbcbe2cfc88153781e4416f86b1
Sanjay <104961183+Sanjay127@users.noreply.github.com>
2023-10-09 10:05:11 +0530

    Add files via upload

---
+++ Day -11/src/main/java/com/mindunits/sanjay/controller/ServiceProviderController.java
@@ -0,0 +1,5 @@
+package com.mindunits.sanjay.controller;
+
+public class ServiceProviderController {
+
+}
---
+++ Day -11/src/main/java/com/mindunits/sanjay/model/Categories.java
@@ -0,0 +1,32 @@
+package com.mindunits.sanjay.model;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "_category")
+
+public class Categories {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
+	private Long id;
+		
+	@Column(length = 50, nullable = false)
+	private String category_name;
+		
+	@Column(length = 50, nullable = false)
+	private String service_name;
+	
+	
+	
+	
+	}
+
+
